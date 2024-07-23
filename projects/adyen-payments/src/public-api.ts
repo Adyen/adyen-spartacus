b3b00b88b884ff5f10a781872c2c38ea6da391f7
kpieloch <113994423+kpieloch@users.noreply.github.com>
2024-07-23 18:11:47 +0200

    Build fix

--- projects/adyen-payments/src/public-api.ts
+++ projects/adyen-payments/src/public-api.ts
@@ -5,7 +5,6 @@
 export * from './lib/adyen-payments.module';
 export * from './lib/root/checkout-adyen-root.module';
 export * from './lib/checkout-adyen-payment-method/checkout-adyen-payment-method.component';
-export * from './lib/checkout-adyen-payment-method/checkout-adyen-payment-form/checkout-adyen-payment-form.component';
 export * from './lib/order/components/order-confirmation/order-confirmation-payment-status/order-confirmation-payment-status.module';
 export * from './lib/order/components/order-confirmation/order-confirmation-payment-status/order-confirmation-payment-status.component';
 export * from './lib/service/adyen-order.service';
