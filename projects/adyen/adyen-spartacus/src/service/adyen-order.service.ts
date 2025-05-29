9e2875553e7b3d3f4771aca350cac8a6d099bc07
kpieloch <113994423+kpieloch@users.noreply.github.com>
2025-05-29 15:16:29 +0200

    AD-361: STOREFRONT_TYPE

--- projects/adyen/adyen-spartacus/src/service/adyen-order.service.ts
+++ projects/adyen/adyen-spartacus/src/service/adyen-order.service.ts
@@ -19,7 +19,7 @@ import {AddressData, PlaceOrderRequest, PlaceOrderResponse} from "../core/models
 import {HttpErrorResponse} from "@angular/common/http";
 import {AdditionalDetailsConnector} from "../core/connectors/additional-details.connector";
 import {errorCodePrefix} from "../assets/translations/translations";
-import {STOREFRONT_VERSION} from '../package.const';
+import {STOREFRONT_VERSION, STOREFRONT_TYPE} from '../package.const';
 
 @Injectable()
 export class AdyenOrderService extends OrderService {
@@ -173,7 +173,7 @@ export class AdyenOrderService extends OrderService {
   static preparePlaceOrderRequest(paymentData: any, billingAddress?: Address): PlaceOrderRequest {
     return {
       paymentRequest: paymentData,
-      storefrontType: "SPARTACUS",
+      storefrontType: STOREFRONT_TYPE,
       storefrontVersion: STOREFRONT_VERSION,
       useAdyenDeliveryAddress: billingAddress === undefined,
       billingAddress: this.mapBillingAddress(billingAddress),
