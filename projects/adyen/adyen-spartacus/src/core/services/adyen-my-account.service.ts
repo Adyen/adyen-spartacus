1859058e5ab30d5c2b8c2fd4645c17c3f5cfd9c0
kpieloch <113994423+kpieloch@users.noreply.github.com>
2026-04-16 18:45:46 +0200

    AD-508: Added functionality to save cards in stored cards component
    (cherry picked from commit 5c4cc93fd0d7af86f07fed6b445fb602a2226c71)

--- projects/adyen/adyen-spartacus/src/core/services/adyen-my-account.service.ts
+++ projects/adyen/adyen-spartacus/src/core/services/adyen-my-account.service.ts
@@ -4,28 +4,40 @@ import { ActiveCartFacade } from "@spartacus/cart/base/root";
 import { UserIdService } from "@spartacus/core";
 import {Observable, switchMap } from "rxjs";
 import {AdyenMyAccountConnector} from "../connectors/adyen-my-account.connector";
-import {AdyenRecurringDetail} from "../models/occ.my-account.models";
+import {StoredPaymentMethodResource, ZeroAuthRequestBody, ZeroAuthResponse} from "../models/occ.my-account.models";
+import { CheckoutConfigurationConnector } from "../connectors/checkout-configuration.connector";
+import { AdyenConfigData } from "../models/occ.config.models";
+import { AdditionalDetailsConnector } from "../connectors/additional-details.connector";
 
 @Injectable()
 export class AdyenMyAccountService extends AdyenBaseService{
   constructor(protected override activeCartFacade: ActiveCartFacade,
               protected override userIdService: UserIdService,
-              protected adyenMyAccountConnector: AdyenMyAccountConnector) {
+              protected adyenMyAccountConnector: AdyenMyAccountConnector,
+              protected checkoutConfigurationConnector: CheckoutConfigurationConnector,
+              protected additionalDetailsConnector: AdditionalDetailsConnector) {
     super(userIdService, activeCartFacade);
   }
 
-  getStoredCards(): Observable<AdyenRecurringDetail[]> {
-    return this.checkoutPreconditions().pipe(
-      switchMap(([userId])=>
+getStoredCards(): Observable<StoredPaymentMethodResource[]> {
+  return this.checkoutPreconditions().pipe(
+    switchMap(([userId]) =>
       this.adyenMyAccountConnector.getStoredCards(userId))
-    )
-  }
+  )
+}
 
   removeStoredCard(cardId: string): Observable<Object> {
-    return this.checkoutPreconditions().pipe(
-      switchMap(([userId])=>
-        this.adyenMyAccountConnector.removeStoredCard(userId, cardId))
-    )
+return this.checkoutPreconditions().pipe(
+    switchMap(([userId]) =>
+      this.adyenMyAccountConnector.removeStoredCard(userId, cardId))
+  )  }
+
+  zeroAuth(requestBody: ZeroAuthRequestBody): Observable<ZeroAuthResponse> {
+    return this.adyenMyAccountConnector.zeroAuth(requestBody);
+  }
+
+  getCheckoutConfiguration(userId: string): Observable<AdyenConfigData> {
+    return this.checkoutConfigurationConnector.getMyAccountCheckoutConfiguration(userId);
   }
 
 }
